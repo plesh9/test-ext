c0e3982e4404160542499d283c4b9c8d3b5eadc9
Kashchenko Vadym <game.plesh9@gmail.com>
2025-03-23 03:12:30 +0200

    fix

--- _scripts/bump-version.ts
+++ _scripts/bump-version.ts
@@ -3,14 +3,12 @@ const pkgPath = "./package.json";
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 
 const isDev = process.argv.includes("--dev");
-const [major, minor, patchWithDev] = pkg.version.split(".");
-let [patch, devBuild] = patchWithDev.split("-dev.");
-
-patch = parseInt(patch);
-devBuild = isDev ? parseInt(devBuild ?? "0") + 1 : 0;
+const [version, devBuild] = pkg.version.split("-dev.");
+const [major, minor, patch] = version.split(".").map(Number)
+const devPatch = isDev ? parseInt(devBuild ?? "0") + 1 : 0;
 
 if (isDev) {
-  pkg.version = `${major}.${minor}.${patch}-dev.${devBuild}`;
+  pkg.version = `${major}.${minor}.${patch}-dev.${devPatch}`;
 } else {
   if (patch >= 99) {
     pkg.version = `${major}.${parseInt(minor) + 1}.0`;
